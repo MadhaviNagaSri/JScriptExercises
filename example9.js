54773e0c54689ad768c0688ba9d4fe59f0eb1436
MadhaviNagaSri <147925584+MadhaviNagaSri@users.noreply.github.com>
2024-03-04 14:43:12 +0530

    Create example9.js

---
+++ example9.js
@@ -0,0 +1,9 @@
+/*calculate a 20% tax for example7*/
+
+let toaster = 80 * 18.50;
+let tshrit = 2 * 80 * 7.50;
+total = toaster + tshrit;
+
+console.log(total);
+/* 20/100 = 0.2 */
+console.log(total*0.2);
