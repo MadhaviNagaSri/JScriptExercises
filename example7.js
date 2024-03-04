ab02f684424317d8621e454c7f6d213a5193df2e
MadhaviNagaSri <147925584+MadhaviNagaSri@users.noreply.github.com>
2024-03-04 14:35:10 +0530

    Create example7.js

---
+++ example7.js
@@ -0,0 +1,6 @@
+/*calculate the total cost of a toaster($18.50) and 2 t-shrits($7.50) each*/
+
+let toaster = 80 * 18.50;
+let tshrit = 2 * 80 * 7.50;
+total = toaster + tshrit;
+console.log(total);
