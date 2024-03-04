5c4d3e1167f7a6a759377f7a2d6324c24c0d40b0
MadhaviNagaSri <147925584+MadhaviNagaSri@users.noreply.github.com>
2024-03-04 15:13:26 +0530

    Create example11.js

---
+++ example11.js
@@ -0,0 +1,17 @@
+/*farenheat = (celcius * 9/5) + 32
+celcius = (farenheat -32) * 5/9 */
+
+/* temperature is 25c.calculate the temperature into farenheat */
+let temperature = 25;
+let farenheat = (temperature * 9/5) + 32;
+console.log(farenheat);
+
+/* farenheat is 86f. calculate the farenheat into temperature */
+let farenheat = 86;
+let temperature = (farenheat-32) * 5/9;
+console.log(temperature);
+
+/* temperature is -5 and calculate the temperature to farenheat */
+let temperature = -5;
+let  farenheat = (temperature * 9/5)+32;
+console.log(farenheat);
